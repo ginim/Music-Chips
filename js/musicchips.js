eecf582fd30377708083e8cff4befe9084ae8ba4
gini <gini.mcgee@gmail.com>
2013-08-11 06:42:35 -0700

    Back to Color Thief 1.0, externalizing to musicchips.js, musicchipsv2
    After testing out Color Thief 2.0, there are too many little weird bugs
    with current code… going back to 1.0 (will revisit 2.0 later after
    getimagedata issue resolved); adding musicchips.js, new html

--- js/musicchips.js
+++ js/musicchips.js
@@ -1,5 +1,72 @@
+
+// Create palette from selected album cover
+var getCover = function() {
+	return createPalette($('.thiscover'), 6); // 2nd argument sets # of colors in palette
+}
+
+// RGB to HEX base function
+// http://stackoverflow.com/questions/1740700/get-hex-value-rather-than-rgb-value-using-jquery
+function rgbArray2hex(rgbArray) {
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+	return "#" + hex(rgbArray[0]) + hex(rgbArray[1]) + hex(rgbArray[2]);
+}
+
+// Convert RGB Array to Hex Array
+var convertPalette = function(rgbArray) {
+	var hexArray = [];
+	for (var i=0; i < 6; i++) {
+		hexArray[i] = rgbArray2hex(rgbArray[i]);
+	}
+	return hexArray;
+}
+
+var showDetails = function(hexArray) {
+	for (var i=0; i < 6; i++) {
+		$("#mini_palette"+i+" .miniswatch").css("background", hexArray[i]);
+		$("#mini_palette"+i+" span").html(hexArray[i]);
+	}
+}
+
+var useCover = function(paletteArray) {
+	for (var i=0; i < 6; i++) {
+		$("#palette"+i).css("background", paletteArray[i]); // Update Swatches
+	}
+	// Update Example
+	$("#palette_example").css("background", paletteArray[0]);
+	$("#palette_example").css("color", paletteArray[1]);
+	$("#palette_example h2").css("color", paletteArray[2]);
+	$("#palette_example h3").css("color", paletteArray[3]);
+	$("#palette_example a").css("color", paletteArray[4]);
+	$("#palette_example button").css("background", paletteArray[5]);
+}
+
 $("body").delegate(".cover", "click", function(){
 	console.log('clicked cover');
 	$("img").removeClass("thiscover");
 	$(this).addClass("thiscover");
+	
+	var rgbaPalette = getCover();
+	// Use RGB palette array from getCover
+	
+	var hexPalette = convertPalette(rgbaPalette);
+	// Get hex palette array by converting RGB array with convertPalette
+	
+	useCover(hexPalette);
+	showDetails(hexPalette);	
+	// Run useCover function, using hexPalette array
+});
+
+$("#details").click(function() {
+	if($(".thiscover").length > 0) {
+		var rgbaPalette = getCover();
+		// Use RGB palette array from getCover
+		var hexPalette = convertPalette(rgbaPalette);
+		// Get hex palette array by converting RGB array with convertPalette
+
+		showDetails(hexPalette);
+		$("#palette_example").hide();
+		$("#palette_details").show();
+	}
 });
